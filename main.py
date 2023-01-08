83b9fe3216089fe0d1602951a98d1b6457fcb7a8
Daniel <daniel.jank@protonmail.com>
2023-01-08 15:34:46 +0000

    main.py: save results to file and set buffer length to default

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ print(f"Connected: {ser.isOpen()}")
 data = []
 counter = 0
 
+answers = []
+temp_answers = []
+
 
 def auto_crc(message):
     crc = (calc.checksum(message))
@@ -54,16 +57,24 @@ payload = None
 
 def p(a, b, c, pl):
     pl = bytes.hex(pl)
-    print(f"[{round(counter * 100 / possibilities)}% {counter}/{possibilities}] Trying 0x{pl}...", end='\r')
+    pl = ' '.join([pl[i:i + 2] for i in range(0, len(pl), 2)])
+    print(f"[{round(counter * 100 / possibilities)}% {counter}/{possibilities}] Trying {pl}...", end='\r')
 
 
 async def watch():
-    global data
+    global data, temp_answers
     while (True):
-        res = [hex(x) for x in ser.read(14)]
+        res = [hex(x) for x in ser.read(47)]
         diffs = check_output(res)
         if diffs > 5:
-            print(f"Possible match found (0x{bytes.hex(payload)}):  gave completely different response")
+            n = ''.join(res).replace("0x", " ")
+            pl = bytes.hex(payload)
+            f = open("combinations.txt", "a")
+            f.write(pl + "\n")
+            f.close()
+            pl = ' '.join([pl[i:i + 2] for i in range(0, len(pl), 2)])
+            print(f"Possible match found ({pl}):  got {n}")
+            answers.append(str(res))
         data = res
         await asyncio.sleep(0.25)
 
@@ -72,12 +83,12 @@ async def watch():
 ################################################### HERE THE MAGIC HAPPENS ###############################################################
 
 
-
 # Brute force ranges for [a, b, c].
 # 255 means 0 to 255 (0x00 to 0xFF) numbers to be checked. 0 means no brute-forcing for specified variable. 1 means brute-forcing 0 and 1
 # You can pass hex values too, like this:
 # loops = [0xFF, 0x01, 0x00] # same as below
 loops = [255, 1, 0]
+
 
 # Just for progress calculation
 brutes = [x + 1 for x in loops]
@@ -89,10 +100,10 @@ async def bruteforce():
     for a in range(brutes[0]):
         for b in range(brutes[1]):
             for c in range(brutes[2]):
+
                 # our hex command. a, b and c are variables to be checked
                 # hex values combined with number operations also are possible, like this:
                 # message = [a, 0x43, b, 0x13, 0x00, 0x01, 0xF0 + c] # useful when c is 0 or 1
-
                 # this bruteforces all commands with 0-1 values
                 # NOTE: do not include the last two bytes (CRC checksum) as it is automatically calculated
                 message = [0x46, 0x43, 0x16, a, 0x00, 0x01, 0xF0 + b]
@@ -104,18 +115,22 @@ async def bruteforce():
                 ser.write(payload)
                 await asyncio.sleep(0.25)
 
+    print()
+    raise SystemExit
+
 
 
 ########################################################################################################################################
 
 
 
-# main, nothing interesting
 async def main():
+    f = open("combinations.txt", "w")
+    f.write("# Possible combinations list, only half of them might work\n")
+    f.close()
     await asyncio.gather(
-        watch(),
         bruteforce(),
+        watch(),
     )
-
 
 asyncio.run(main())
